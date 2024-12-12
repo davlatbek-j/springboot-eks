c0419da693cbc4827b1a225b9abf8f0256d83e96
SuperHuma <theferganes@gmail.com>
2024-12-12 23:41:58 +0500

    new year

--- src/main/java/com/javatechie/SpringBootEksApplication.java
+++ src/main/java/com/javatechie/SpringBootEksApplication.java
@@ -7,15 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringBootEksApplication {
+public class SpringBootEksApplication
+{
 
 
-    @GetMapping("/greetings")
-    public String message() {
-        return "Welcome to javatechie ! app deployed in AWS EKS";
+    @GetMapping("/")
+    public String message()
+    {
+        return "Happy New Year !!! app deployed in AWS EKS";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(SpringBootEksApplication.class, args);
     }
 
